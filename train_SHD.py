b57788950a187f525c2a4b1527dc29bdbd7143bb
RRK37 <ricus.klotins@gmail.com>
2025-05-14 13:08:33 +0100

    New training set-up

--- train_SHD.py
+++ train_SHD.py
@@ -6,17 +6,13 @@ import numpy as np
 import json
 from os.path import exists
 
-
+# Training Setup
 p["EVALUATION"] = "speaker"
 p["N_EPOCH"] = 300
-p["LOSS_TYPE"] = "sum_weigh_exp"
-p["MIN_EPOCH_ETA_FIXED"] = 300
 p["BALANCE_TRAIN_CLASSES"]= True
 p["BALANCE_EVAL_CLASSES"]= True
-
 p["TRAIN_DATA_SEED"] = 123
 p["TEST_DATA_SEED"] = 321
-p["REG_TYPE"] = "simple"
 p["TRIAL_MS"] = 1000.0
 p["AUGMENTATION"]= {
     "NORMALISE_SPIKE_NUMBER": True,
@@ -26,12 +22,21 @@ p["AUGMENTATION"]= {
 p["N_INPUT_DELAY"] = 10
 p["INPUT_DELAY"] = 30
 
+
+# Learning parameters
+p["ETA"] = 0.001
+p["MIN_EPOCH_ETA_FIXED"] = 300
+p["LOSS_TYPE"] = "sum_weigh_exp"
+p["TAU_0"] = 1
+p["TAU_1"] = 100
+p["ALPHA"] = 5*10^(-5)
+# p["GLB_UPPER"] = 10^(-9)
+
+
+# Network parameters
+p["REG_TYPE"] = "simple"
 p["TAU_MEM"] = 20
 p["TAU_SYN"] = 5
-# p["GLB_UPPER"] = 10^(-9)
-p["ETA"] = 0.001
-
-
 p["N_HID_LAYER"] = 1
 p["NUM_HIDDEN"] = 1024
 p["RECURRENT"] = True
@@ -44,14 +49,10 @@ p["HIDDEN_OUTPUT_STD"] = 0.03
 p["PDROP_INPUT"] = 0
 p["NU_UPPER"] = 14
 
-p["TAU_0"] = 1
-p["TAU_1"] = 100
-p["ALPHA"] = 5*10^(-5)
 
-
-p["BUILD"] = True
-
+# Recording parameters
 p["OUT_DIR"] = "z_bell_loss_0_-02_44_04"
+
 
 #p["DEBUG"]= True
 #p["REC_SPIKES"]= ["input","hidden"]
@@ -65,6 +66,11 @@ p["OUT_DIR"] = "z_bell_loss_0_-02_44_04"
 #p["REC_SYNAPSES_EPOCH_TRIAL"] = [1,7]
 #p["W_OUTPUT_EPOCH_TRIAL"] = [1,7]
 #p["TAU_OUTPUT_EPOCH_TRIAL"] = [1,7]
+
+
+
+p["BUILD"] = True
+
 
 jname= os.path.join(p["OUT_DIR"], p["NAME"]+".json")
 jfile= open(jname,'w')
