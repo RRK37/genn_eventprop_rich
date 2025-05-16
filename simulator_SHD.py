ff3a63b614bc30a3c417639c788b303b74ce9c88
RRK37 <ricus.klotins@gmail.com>
2025-05-16 11:02:11 +0100

    new train

--- simulator_SHD.py
+++ simulator_SHD.py
@@ -1960,7 +1960,10 @@ class SHD_model:
                                         spike_N_hidden[l][btch]+= len(x[btch][0])
 
                     if len(p["REC_SPIKES"]) > 0:
+                        print(int_t)
+                        print(p["SPK_REC_STEPS"])
                         if int_t%p["SPK_REC_STEPS"] == 0:
+                            print("yes")
                             if [epoch,trial] in p["REC_SPIKES_EPOCH_TRIAL"]:
                                 self.model.pull_recording_buffers_from_device()
                                 for pop in p["REC_SPIKES"]:
