c6c4dd594988c7214c488ba192cdbef21a0b97d3
RRK37 <ricus.klotins@gmail.com>
2025-05-21 11:31:59 +0100

    new

--- train_SHD.py
+++ train_SHD.py
@@ -97,6 +97,8 @@ p["BUILD"] = True
 # print("correct_eval: {}".format(correct_eval))
 
 
+p["BALANCE_TRAIN_CLASSES"] = False
+p["BALANCE_EVAL_CLASSES"] = False
 p["LOAD_BEST"] = True
 p["BUILD"] = False
 p["AUGMENTATION"]= {
