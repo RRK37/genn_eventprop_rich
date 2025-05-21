962e26322c0d7aa23e9b728f7a3b6fbca69c7857
RRK37 <ricus.klotins@gmail.com>
2025-05-21 19:06:04 +0100

    new

--- simulator_SHD.py
+++ simulator_SHD.py
@@ -2241,6 +2241,11 @@ class SHD_model:
                     confusion[ph].append(conf[ph])
 
             if (p["CHECKPOINT_BEST"] == "validation" and correct_eval > correct_eval_best) or (p["CHECKPOINT_BEST"] == "training" and correct > correct_best):
+                
+                print("saved best")
+                print("saved best")
+                print("saved best")
+                
                 correct_eval_best = correct_eval
                 correct_best = correct
                 with open(os.path.join(p["OUT_DIR"], p["NAME"]+"_best.txt"),"w") as f:
