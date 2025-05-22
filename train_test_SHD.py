6a6ad950667e378ce8ffd63b1e9ad0dc2c95dce5
RRK37 <ricus.klotins@gmail.com>
2025-05-22 20:36:40 +0100

    new

--- train_test_SHD.py
+++ train_test_SHD.py
@@ -3,54 +3,83 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 
-p["TRIAL_MS"]= 1400
+
+
+# Training Setup
+p["EVALUATION"]             = "speaker"
+p["N_EPOCH"]                = 3
+p["BALANCE_TRAIN_CLASSES"]  = True
+p["BALANCE_EVAL_CLASSES"]   = True
+p["TRAIN_DATA_SEED"]        = 32
+p["TEST_DATA_SEED"]         = 44
+p["TRIAL_MS"]               = 1000.0
+p["AUGMENTATION"]= {
+    "NORMALISE_SPIKE_NUMBER": True,
+    "random_shift": 40.0,
+    "blend": [0.5,0.5]
+}
+p["N_INPUT_DELAY"]          = 10
+p["INPUT_DELAY"]            = 30
+p["N_BATCH"]= 32
 p["DATASET"]= "SHD"
-p["TRAIN_DATA_SEED"]= 372
-p["TEST_DATA_SEED"]= 814
 p["NAME"]= "tt1"
-p["NUM_HIDDEN"]= 256
-p["N_MAX_SPIKE"]= 1500
 p["DT_MS"]= 1
-p["PDROP_INPUT"]= 0.1
-p["PDROP_HIDDEN"]= 0.0
-p["ADAM_BETA1"]= 0.9
-p["ADAM_BETA2"]= 0.999   
 p["DEBUG"]= False
 p["DEBUG_HIDDEN_N"]= True
 p["LOAD_LAST"]= False
-p["N_EPOCH"]= 2
+
+
+# Learning parameters
+p["ETA"]                    = 0.001
+p["MIN_EPOCH_ETA_FIXED"]    = 1
+p["LOSS_TYPE"]              = "sum_weigh_exp"
+p["TAU_0"]                  = 1
+p["TAU_1"]                  = 100
+p["ALPHA"]                  = 5*10^(-5)
+
+
+# Network parameters
+p["REG_TYPE"]               = "simple"
+p["TAU_MEM"]                = 20
+p["TAU_SYN"]                = 5
+p["N_HID_LAYER"]            = 1
+p["NUM_HIDDEN"]             = 1024
+p["RECURRENT"]              = True
+p["INPUT_HIDDEN_MEAN"]      = 0.03
+p["INPUT_HIDDEN_STD"]       = 0.01
+p["HIDDEN_HIDDEN_MEAN"]     = 0
+p["HIDDEN_HIDDEN_STD"]      = 0.02 
+p["HIDDEN_OUTPUT_MEAN"]     = 0 
+p["HIDDEN_OUTPUT_STD"]      = 0.03
+p["PDROP_INPUT"]            = 0
+p["NU_UPPER"]               = 14
+p["GLB_UPPER"]              = 1e-9
+p["LBD_UPPER"]              = 2e-9
+p["LBD_LOWER"]              = 2e-9
+p["N_MAX_SPIKE"]= 1500
+p["PDROP_HIDDEN"]= 0.0
+p["ADAM_BETA1"]= 0.9
+p["ADAM_BETA2"]= 0.999   
+
+
+
 p["N_BATCH"]= 256
 p["SUPER_BATCH"]= 1
 p["N_TRAIN"]= 8156 # that is all of them
 p["N_VALIDATE"]= 0 # no validation
-p["ETA"]= 5e-3 #5e-3
 p["SHUFFLE"]= True
-p["INPUT_HIDDEN_MEAN"]= 0.02 # 0.02
-p["INPUT_HIDDEN_STD"]= 0.01 # 0.01
-p["HIDDEN_OUTPUT_MEAN"]= 0.0
-p["HIDDEN_OUTPUT_STD"]= 0.03 # 0.3
 p["W_REPORT_INTERVAL"] = 11000  # this should be at the end of the epoch (at first trial of evaluation)
-p["TAU_MEM"] = 20.0 #20
-p["TAU_SYN"] = 5.0 #5
-p["REG_TYPE"]= "simple"
-p["LBD_UPPER"]= 1e-12 # 5e-12 keep in mind that the term is applied to all contributing spikes ...
-p["LBD_LOWER"]= 1e-5
-p["NU_UPPER"]= 15 #*p["N_BATCH"]
-p["NU_LOWER"]= 5
-p["RHO_UPPER"]= 10000.0
-p["GLB_UPPER"]= 1e-8
-p["ETA_DECAY"]= 1.0      
+# p["NU_LOWER"]= 5
+# p["RHO_UPPER"]= 10000.0
+# p["ETA_DECAY"]= 1.0      
 p["ETA_FIDDELING"]= False
 p["ETA_REDUCE"]= 0.5
 p["ETA_REDUCE_PERIOD"]= 50
 p["TIMING"]= False
 p["SPK_REC_STEPS"]= int(p["TRIAL_MS"]/p["DT_MS"])
-p["LOSS_TYPE"]= "sum"
 p["EVALUATION"]= "random"
 
 p["RECURRENT"]= True
-p["HIDDEN_HIDDEN_MEAN"]= 0.0
-p["HIDDEN_HIDDEN_STD"]= 0.02 # 0.02
 
 p["REWIRE_SILENT"]= True
 p["AVG_SNSUM"]= True
